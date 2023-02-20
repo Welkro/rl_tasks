eab146aa3221137c7367305a432a8cc39d65423d
Welkro <royliu29@gmail.com>
2023-02-20 11:27:12 +0200

    Completed JavaScript

--- layout/ex1.php
+++ layout/ex1.php
@@ -1,5 +1,6 @@
 <?php $title = "Exercise 1"; 
-include "header.php"; ?>
+include "header.php";?>
+
 <div class="main">
     <div class="txtblock">
     <hr><h1> 3.1 Write a simple PHP script to print your information (Name and your groupID).</h1>
@@ -14,12 +15,27 @@ include "header.php"; ?>
     <?php
     echo "Hello World! My name is \"Roy\"";
     ?>
-
+    <script>
+    //window.alert
+    //window.alert("This will trigger an alert box");
+    //window.alert(5+8);
+    //document.write
+    document.write("Hello this is interesting");
+    </script>
 <hr><h1> 3.3 Write the PHP code in to display the current date. </h1>
 
     <?php
     echo date('d.M.Y');
     ?>
+<br>
+    <script>
+        document.write("Hello, this is JavaScript")
+    </script>
+
+    <noscript>
+        Please enable JavaScript to view this content
+    </noscript>
+    <input type="button" onclick="hello()" value="Click Me!">
 
 <hr><h1> 3.4 Variables in PHP </h1>
 
@@ -29,7 +45,13 @@ include "header.php"; ?>
     ?>
 
 <hr><h1> 3.5 Table & Variables </h1>
-
+<button onclick="add()"> Calculate 5+5 </button>
+<p id="place1" style="color:red; background-color:white;"> </p>
+<span id="place2" style="color:purple; background-color:violet;"> </span>
+<script>
+document.getElementById("place1").innerHTML="This will go to place1"
+document.getElementById("place2").innerHTML="This will go to span"
+</script>
     <?php
     $g1 = 5;
     $g2 = 4;
@@ -51,6 +73,12 @@ include "header.php"; ?>
 
 <hr><h1> 4. Screenshot of the development environment </h1>
 <img src="proof.jpg" alt="image">
+
+<hr><h1> Changing background-color </h1>
+<form>
+<input type="color" name="background" onchange="setColor('background', this.value)">
+</form>
+
     </div>
 </div>
 <?php include "footer.php"; ?>
---
+++ layout/js/site.js
@@ -0,0 +1,18 @@
+// a simple function that triggers alert box
+function hello(){
+    alert("Welcome to my website!");
+}
+
+// a simple function to add 2 numbers
+function add(){
+    document.write(5+5);
+}
+//document.getElementById(id)
+
+//function to change the background of the page 
+function setColor(where,newColor){
+    if(where == "background"){
+        document.body.style.backgroundColor = newColor;
+
+    }
+}
